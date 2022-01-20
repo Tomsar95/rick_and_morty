5e70cea2f14604869bf7634df09607598ecafb44
Tomas Sarmiento <t.sarmiento@loro.com.co>
2022-01-20 18:40:13 -0500

    dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,54 @@
+import 'package:get_it/get_it.dart';
+import 'package:rick_and_morty/features/characters/data/datasources/characters_remote_data_source.dart';
+import 'package:rick_and_morty/features/characters/data/repositories/characters_repository_impl.dart';
+import 'package:rick_and_morty/features/characters/domain/repositories/characters_repository.dart';
+import 'package:rick_and_morty/features/characters/presentation/bloc/characters/characters_bloc.dart';
+
+import 'features/characters/domain/use_cases/get_characters.dart';
+import 'features/characters/domain/use_cases/get_concrete_character.dart';
+import 'features/characters/presentation/bloc/character/character_bloc.dart';
+
+import 'package:http/http.dart' as http;
+
+final serviceLocator = GetIt.instance;
+
+Future<void> init() async {
+  // Features - Number Trivia
+  initFeatures();
+  // Core
+  initCore();
+  // External
+  await initExternal();
+}
+
+void initFeatures() {
+  // BLoC
+  serviceLocator.registerFactory(() => CharactersBloc(
+      getCharacters: serviceLocator()));
+  serviceLocator.registerFactory(() => CharacterBloc(
+      getConcreteCharacter: serviceLocator()));
+
+  // Use Cases
+  serviceLocator
+      .registerLazySingleton(() => GetCharacters(serviceLocator()));
+  serviceLocator
+      .registerLazySingleton(() => GetConcreteCharacter(serviceLocator()));
+
+  // Repository
+  serviceLocator.registerLazySingleton<CharactersRepository>(() =>
+      CharactersRepositoryImpl(
+          remoteDataSource: serviceLocator()));
+
+  // Data sources
+  serviceLocator.registerLazySingleton<CharactersRemoteDataSource>(
+          () => CharactersRemoteDataSourceImpl(client: serviceLocator()));
+}
+
+void initCore() {
+  // input converter
+  // network info
+}
+
+Future<void> initExternal() async {
+  serviceLocator.registerLazySingleton(() => http.Client());
+}
